60d9d3459de8441a7fd47a2a35bb76f285d5a89c
Ja-rek <jk@Ja-Rek.localdomain>
2019-01-16 23:43:37 +0100

    Added Linq extensions.

---
+++ Monads/Either/Extensions/Linq/BindInternalEitherExtension.cs
@@ -0,0 +1,40 @@
+using System;
+using Monads.Common;
+using static Monads.Either.EitherFactory;
+
+namespace Monads.Either.Linq
+{
+    internal static class BindInternalMaybeExtension
+    {
+        public static Either<TLeft, TResult> Bind<TLeft, TRight, TResult>(
+            this Either<TLeft, TRight> source,
+            Func<TRight, TResult> predicate)
+        {
+            EitherAssert.LeftOrRightExist(source);
+
+            if (source.IsRight())
+            {
+                var predicateArg = predicate(source.Right);
+
+                Assert.ArgumentIsNotNull(predicateArg, nameof(predicateArg));
+
+                return Right(predicateArg);
+            }
+
+            return Left(source.Left);
+        }
+
+        public static Either<TLeft, TResult> Bind<TLeft, TRight, TResult>(
+            this Either<TLeft, TRight> source,
+            Func<TRight, Either<TLeft, TResult>> predicate)
+        {
+            EitherAssert.LeftOrRightExist(source);
+
+            var predicateArg = predicate(source.Right);
+
+            EitherAssert.LeftOrRightExist(predicateArg);
+
+            return source.IsRight() ? predicateArg : Left(source.Left);
+        }
+    }
+}
