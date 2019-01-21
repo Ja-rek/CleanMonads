4a5c29fbd072f1c9bd80904f641e35d72dd257fe
Ja-rek <jk@Ja-Rek.localdomain>
2019-01-21 03:16:05 +0100

    Added tests of 'ToEither' and did some refactoring.

--- Monads.Tests/Either/Extensions/CastTest.cs
+++ Monads.Tests/Either/Extensions/CastTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Monads.Either;
+using Monads.Either.Unsafe;
 
 namespace Monads.Tests.Either.Extensions
 {
@@ -8,7 +9,7 @@ namespace Monads.Tests.Either.Extensions
         [Test]
         public void ToRightIf_WhenConditionIsMet_ReturnRightEither()
         {
-            var actual = int_10.ToRightIf(x => x == 10, str_Error);
+            var actual = int_10.ToRightIf(x => x == 10, () => str_Error);
 
             Assert.AreEqual(rightInt_10, actual);
         }
@@ -16,7 +17,7 @@ namespace Monads.Tests.Either.Extensions
         [Test]
         public void ToRightIf_WhenConditionIsNotMet_ReturnLeftEither()
         {
-            var actual = int_10.ToRightIf(x => x != 10, str_Error);
+            var actual = int_10.ToRightIf(x => x != 10,() => str_Error);
 
             Assert.AreEqual(leftStr_Error, actual);
         }
@@ -24,7 +25,7 @@ namespace Monads.Tests.Either.Extensions
         [Test]
         public void ToLeftIf_WhenConditionIsMet_ReturnLeftEither()
         {
-            var actual = int_10.ToLeftIf(x => x == 10, str_Error);
+            var actual = int_10.ToLeftIf(x => x == 10,() => str_Error);
 
             Assert.AreEqual(leftStr_Error, actual);
         }
@@ -32,7 +33,47 @@ namespace Monads.Tests.Either.Extensions
         [Test]
         public void ToLeftIf_WhenConditionIsNotMet_ReturnRightEither()
         {
-            var actual = int_10.ToLeftIf(x => x != 10, str_Error);
+            var actual = int_10.ToLeftIf(x => x != 10,() => str_Error);
+
+            Assert.AreEqual(rightInt_10, actual);
+        }
+
+        [Test]
+        public void ToEither_WhenNullableRightHasValue_ReturnRightEither()
+        {
+            var actual = nullableInt_10.ToEither(() => str_Error);
+
+            Assert.AreEqual(rightInt_10, actual);
+        }
+
+        [Test]
+        public void ToEither_WhenNullableSourceIsNull_ReturnLeftEither()
+        {
+            var actual = nullableInt_Null.ToEither(() => str_Error);
+
+            Assert.AreEqual(leftStr_Error, actual);
+        }
+
+        [Test]
+        public void ToEither_WhenSourceHasValue_ReturnRightEither()
+        {
+            var actual = nullableInt_10.ToEither(() => str_Error);
+
+            Assert.AreEqual(rightInt_10, actual);
+        }
+
+        [Test]
+        public void ToEither_WhenSurceIsNull_ReturnLeftEither()
+        {
+            var actual = str_Default.ToEither(() => str_Error);
+
+            Assert.AreEqual(str_Error, actual.Left());
+        }
+
+        [Test]
+        public void ToEither_WhenSurceHasValue_ReturnRightEither()
+        {
+            var actual = int_10.ToEither(() => str_Error);
 
             Assert.AreEqual(rightInt_10, actual);
         }
