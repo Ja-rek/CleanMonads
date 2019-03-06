1ea65b163d06930b20d6798c6afccdcec54598ff
Ja-rek <jk@Ja-Rek.localdomain>
2019-03-06 17:38:23 +0100

    Refactor
      - Remove unnecessary namespace of utils
      - Change type 'INothing' on 'NotDefined'

--- Monads/Maybe/Maybe.Operators.cs
+++ Monads/Maybe/Maybe.Operators.cs
@@ -3,7 +3,7 @@ namespace Monads
     public partial struct Maybe<TData> 
     {
         public static implicit operator Maybe<TData>(TData some) => new Maybe<TData>(some);
-        public static implicit operator Maybe<TData>(Maybe<INothing> none) => new Maybe<TData>(false);
+        public static implicit operator Maybe<TData>(Maybe<NotDefined> none) => new Maybe<TData>(false);
 
         public static bool operator ==(Maybe<TData> left, Maybe<TData> right) => left.Equals(right);
         public static bool operator !=(Maybe<TData> left, Maybe<TData> right) => !left.Equals(right);
