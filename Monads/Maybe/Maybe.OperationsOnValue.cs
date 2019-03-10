dbccc224d3b59c7d5d6f43b92505563ae3f3dfb6
Ja-rek <jk@Ja-Rek.localdomain>
2019-03-10 09:44:50 +0100

    Fix bug, refactor

--- Monads/Maybe/Maybe.OperationsOnValue.cs
+++ Monads/Maybe/Maybe.OperationsOnValue.cs
@@ -6,23 +6,36 @@ namespace Monads
     {
         public Maybe<TResult> Map<TResult>(Func<TData, TResult> predicate)
         {
-            if (this.HasValue()) return predicate(this.value);
+            if (this.hasValue) return predicate(this.value);
 
             return new Maybe<TResult>(false);
         }
 
         public Maybe<TResult> FlatMap<TResult>(Func<TData, Maybe<TResult>> predicate)
         {
-            if (this.HasValue()) return predicate(this.value);
+            if (this.hasValue) return predicate(this.value);
 
             return new Maybe<TResult>(false);
         }
 
         public Maybe<TData> Filter(Func<TData, bool> condition)
         {
-            return this.HasValue() | condition(this.value)
-                ? this.value
-                : new Maybe<TData>(false);
+            if (this.hasValue && condition(this.value))
+            {
+                return this.value;
+            }
+
+            return new Maybe<TData>(false);
+        }
+
+        public Maybe<TData> Or(Maybe<TData> alternative)
+        { 
+            return !this.hasValue ? alternative : this;
+        }
+
+        public Maybe<TData> Or(Func<Maybe<TData>> alternativeFactory)
+        {
+            return this.Or(alternativeFactory());
         }
     }
 }
